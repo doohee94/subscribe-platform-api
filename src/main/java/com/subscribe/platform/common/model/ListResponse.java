d1c7709142d95b10d2e4eb7ac6dd664337366ab3
doohee94 <doohee94@naver.com>
2021-04-17 00:08:58 +0900

    ListResponse 모델 생성

---
+++ src/main/java/com/subscribe/platform/common/model/ListResponse.java
@@ -0,0 +1,10 @@
+package com.subscribe.platform.common.model;
+
+import java.util.List;
+
+public class ListResponse<T> extends ListResponseModel<T> {
+
+    public ListResponse(List<T> content) {
+        super(content);
+    }
+}
