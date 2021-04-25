5fc143216365782a37eadfc96b234a862004ca79
maro42 <maro42@naver.com>
2021-04-25 15:35:32 +0900

    api테스트

--- src/main/java/com/subscribe/platform/apiTest/controller/ApiTestController.java
+++ src/main/java/com/subscribe/platform/apiTest/controller/ApiTestController.java
@@ -1,8 +1,7 @@
-package com.subscribe.platform.controller;
+package com.subscribe.platform.apiTest.controller;
 
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
